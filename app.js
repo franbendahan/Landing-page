fc789cf65ade960f071350b3a33333b14954c9fa
unknown <franbendahan@gmail.com>
2022-03-11 19:29:21 -0300

    nuevo header responsive

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
-const btnMenu = document.querySelector("#btnMenu");
-const menu = document.querySelector("#menu");
-
-btnMenu.addEventListener("click", function() {
-    menu.classList.toggle("mostrar");
-});
-
-const subMenuBtn = document.querySelectorAll(".submenu-btn");
-
-for(let i=0; i<subMenuBtn.length; i++) {
-    subMenuBtn[i].addEventListener("click", function(){
-        if(window.innerWidth < 1024) {
-            const subMenu = this.nextElementSibling;
-            const height = subMenu.scrollHeight;
-
-            if(subMenu.classList.contains("desplegar")) {
-                subMenu.classList.remove("desplegar");
-                subMenu.removeAttribute("style")     
-            }  else {
-                subMenu.classList.add("desplegar");
-                subMenu.style.height = height + "px";
-            }
+const menuMobile = document.getElementById('menu-mobile');
+window.addEventListener("click", function(event) {
+    if (event.target.id === "hambur-header-only"){
+        if (menuMobile.className.includes('show')){
+            menuMobile.classList.remove('show');
+        } else {
+            menuMobile.classList.add('show');
         }
-    });
-}
+    }
+})
+
+window.addEventListener("resize", function(event) {
+    if (window.innerWidth >= 768){
+        menuMobile.classList.remove('show');
+    }
+});
 
